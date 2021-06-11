cc225be8f5e9cc8bb9b535f2ff80597fe82d4234
Muhindo Galien <74584848+Muhindo-Galien@users.noreply.github.com>
2021-06-11 16:14:42 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const name = "Muhindo";
+const currentDate = new Date();
+const year = currentDate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copyright {year}</p>
+  </div>,
+  document.getElementById("root")
+);
